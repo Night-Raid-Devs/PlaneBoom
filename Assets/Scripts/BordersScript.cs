0447e50fb0cc0357593a0a01f7963f9adad76d75
vlad_klitsunov <wist214@mail.ru>
2018-03-24 17:07:12 +0200

    Fix border script, add DestroyBorder script.

--- Assets/Scripts/BordersScript.cs
+++ Assets/Scripts/BordersScript.cs
@@ -8,19 +8,21 @@ public class BordersScript : MonoBehaviour {
     private int counter = -1;
     public Text counterText;
     private float updateTime = 0;
-
-	void Start () {
-		
-	}
+    Collider obj;
 	
 	void Update () {
         if (counter != -1)
         {
             if (updateTime >= 1)
-            {
+            {               
                 counterText.text = counter.ToString();
-                counter = counter--;
+                counter--;
                 updateTime = 0;
+
+                if (counter == 0)
+                {
+                    Destroy(obj.gameObject);
+                }
             }
             updateTime += Time.deltaTime;
         }
@@ -30,8 +32,17 @@ public class BordersScript : MonoBehaviour {
     {
         if (other.gameObject.name == "AircraftJet" && counter == -1)
         {
+            obj = other;
             updateTime = 0;
             counter = 5;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "AircraftJet" && counter != -1)
+        {
+            counter = -1;
         }
     }
 }

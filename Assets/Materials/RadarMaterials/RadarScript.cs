2504d5092fca6e8735ed79f2679614f888815096
vlad_klitsunov <wist214@mail.ru>
2018-04-19 20:07:55 +0300

    Fix merge

---
+++ Assets/Materials/RadarMaterials/RadarScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarScript : MonoBehaviour {
+
+    public GameObject borderPrefab;
+    public float switchDistance;
+    public Transform helpTransform;
+    public Transform player;
+    public Transform radarCamera;
+    public string findTag = "radar";
+    GameObject[] trackedObjects;
+    List<GameObject> borderObjects;
+
+	void Start () {
+        CreateBorderObjects();
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        radarCamera.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        if (trackedObjects != null)
+        {
+            for (int i = 0; i < trackedObjects.Length; i++)
+            {
+                var curr2DPos = new Vector2(trackedObjects[i].transform.position.x, trackedObjects[i].transform.position.z);
+                var obj2DPos = new Vector2(transform.position.x, transform.position.z);
+                if (Vector2.Distance(curr2DPos, obj2DPos) > switchDistance)
+                {
+                    var trackedPos = new Vector3(trackedObjects[i].transform.position.x, helpTransform.position.y, trackedObjects[i].transform.position.z);
+                    helpTransform.LookAt(trackedPos);
+                    var newBorderObjectPos = switchDistance * helpTransform.forward;
+                    newBorderObjectPos.y = helpTransform.localPosition.y;
+                    borderObjects[i].transform.localPosition = newBorderObjectPos;
+                    borderObjects[i].layer = LayerMask.NameToLayer("Radar");
+                }
+                else
+                {
+                    borderObjects[i].layer = LayerMask.NameToLayer("Invisible");
+                }
+            }
+        }
+    }
+
+    void CreateBorderObjects()
+    {
+        borderObjects = new List<GameObject>();
+        trackedObjects = UtilsScript.FindGameObjectsWithTag(findTag);
+        foreach (GameObject o in trackedObjects)
+        {
+            GameObject j = Instantiate(borderPrefab, transform);
+            borderObjects.Add(j);
+        }
+    }
+}
---
+++ Assets/Prefabs/Loading/Loading.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class Loading : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Image loadingImg;
+    public float step;
+    bool active = false;
+
+	void Start () {
+       step = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            active = true;
+        }
+        if(active)
+        { 
+            if (step >= 0)
+            {
+                loadingImg.fillAmount = step;
+                step -= 0.005f;
+            }
+            else {active = false; step = 1; }
+        }
+	}
+}

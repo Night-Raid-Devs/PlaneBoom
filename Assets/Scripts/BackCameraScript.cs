7bb5becb2d25075c9bba163d8cd9ca129cad60e5
Maxim <--global>
2018-05-18 03:50:40 +0300

    add back camera script

---
+++ Assets/Scripts/BackCameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BackCameraScript : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera curCamera;
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            curCamera.enabled = true;
+            mainCamera.enabled = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            mainCamera.enabled = true;
+            curCamera.enabled = false;
+        }
+    }
+}

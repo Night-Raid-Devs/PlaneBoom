c16472d8bb153b1e4385e20717fe6fb6efe336cd
Maxim <--global>
2018-05-18 06:08:26 +0300

    add level2, some fix

--- Assets/Scripts/BackCameraScript.cs
+++ Assets/Scripts/BackCameraScript.cs
@@ -8,11 +8,15 @@ public class BackCameraScript : MonoBehaviour {
     void Update () {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            curCamera.tag = "MainCamera";
+            mainCamera.tag = "Untagged";
             curCamera.enabled = true;
             mainCamera.enabled = false;
         }
         else if (Input.GetKeyUp(KeyCode.E))
         {
+            mainCamera.tag = "MainCamera";
+            curCamera.tag = "Untagged";
             mainCamera.enabled = true;
             curCamera.enabled = false;
         }
